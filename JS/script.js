3bbccfe9a48cdf9b2c3cb0780a06cff8f1b2d473
Ali Sadeghi <ali909392@gmail.com>
2024-09-05 19:29:50 +0330

    1403-06-15

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,34 @@
-console.log("Hi");
+// var name = "John";
+
+// function first() {
+//     var a = "Hello!";
+//     second();
+//     var x = a + name;
+//     console.log("1");
+// }
+
+// function second() {
+//     var b = "Hi!";
+//     third();
+//     var z = b + name;
+//     console.log("2");
+// }
+
+// function third() {
+//     var c = "Hey!";
+//     var z = c + name;
+//     console.log("3");
+// }
+
+// first(); // 3 2 1
+
+// "use strict";
+
+function hello() {
+    var hi = 10;
+    console.log(hi);
+}
+
+hello();
+
+console.log("hi >>", hi);
