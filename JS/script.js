bf4f6f22ddf490819ec10cab1b5acc030562b375
Ali Sadeghi <ali909392@gmail.com>
2024-10-09 21:22:56 +0330

    1403-07-18

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,28 @@
 console.log("Hi");
+
+let todoArray = [];
+
+const fetchTodosFromFirebase2 = async () => {
+    try {
+        const response = await fetch(
+            "https://todolist-zahra-default-rtdb.firebaseio.com/todos.json"
+        );
+
+        const data = await response.json();
+
+        if (data) {
+            todoArray = Object.keys(data).map((key) => ({
+                id: key,
+                title: data[key].title,
+                complete: data[key].complete,
+            }));
+
+            console.log("Fetched Todos:", todoArray);
+            todoArray.forEach((todo) => createTodoItem(todo));
+        }
+    } catch (error) {
+        console.error("Error fetching todos:", error);
+    }
+};
+
+fetchTodosFromFirebase2();
