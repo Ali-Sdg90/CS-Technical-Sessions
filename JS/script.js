eb551a2ad7282d981a28d805150dded65a4e4b71
Ali Sadeghi <ali909392@gmail.com>
2024-11-13 21:59:04 +0330

    1403-08-23

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,9 @@
 console.log("Hi");
+
+const hi = document.querySelector(".salam");
+
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", () => {
+    hi.innerHTML = "Salam";
+});
